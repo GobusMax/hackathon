f89b716f1ed3533c114ee7d252f8f7cfa6d534d4
Alexander Thiem <thiemalexander@outlook.de>
2023-05-06 08:39:22 +0200

    asdf

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+use egui::vec2;
 use image::open;
 #[path = "bin/detect.rs"]
 mod detect;
 mod image_manipulation;
+#[path = "bin/visualization.rs"]
+mod vis;
+
 fn main() {
-    let a = open("data/plane_2_1.jpg").unwrap();
+    let a = open("data/plane_2_2.jpg").unwrap();
     let b = open("data/no_plane_2.jpg").unwrap();
-    let mut diff = detect::difference2(a.into_rgb8(), b.into_rgb8());
-    diff.save("Testdiff.jpg");
+    let diff = detect::difference2(&a.clone().into_rgb8(), &b.into_rgb8());
+    diff.save("Difference.jpg").unwrap();
+    let diff = open("Difference.jpg").unwrap();
+    diff.grayscale().save("GrayscaleDifference.jpg").unwrap();
+    let diff = open("GrayscaleDifference.jpg").unwrap();
+    let x = detect::average(&diff);
+    let data = vec![
+        vec2(x.0 as f32, a.height() as f32 - x.1 as f32),
+        vec2(0., 0.),
+    ];
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My egui App",
+        native_options,
+        Box::new(|cc| Box::new(vis::MyEguiApp::new(cc, a, data))),
+    )
+    .unwrap();
     //let a = image_manipulation::to_grayscale(a);
     //let b = image_manipulation::to_grayscale(b);
     //dbg!("Grayscaled");
