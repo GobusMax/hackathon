f7db2286e107c2a21095c1569e484448c885405b
Gobus <florian-c-hoffmann@t-online.de>
2023-05-06 03:14:08 +0200

    image in plot & rustfmt

--- src/bin/visualization.rs
+++ src/bin/visualization.rs
@@ -1,4 +1,7 @@
-use egui::{ColorImage, TextureHandle, TextureOptions};
+use egui::{
+    plot::{Line, Plot, PlotImage, PlotPoints},
+    ColorImage, TextureHandle, TextureOptions, Vec2,
+};
 
 use image::{io::Reader, DynamicImage};
 
@@ -8,15 +11,7 @@ fn main() {
     eframe::run_native(
         "My egui App",
         native_options,
-        Box::new(
-            |cc| {
-                Box::new(
-                    MyEguiApp::new(
-                        cc, img,
-                    ),
-                )
-            },
-        ),
+        Box::new(|cc| Box::new(MyEguiApp::new(cc, img))),
     )
     .unwrap();
 }
@@ -36,10 +31,7 @@ impl MyEguiApp {
         let pixels = image_buffer.as_flat_samples();
         let res = cc.egui_ctx.load_texture(
             "Blub",
-            ColorImage::from_rgba_unmultiplied(
-                size,
-                pixels.as_slice(),
-            ),
+            ColorImage::from_rgba_unmultiplied(size, pixels.as_slice()),
             TextureOptions::default(),
         );
         Self { tex: res }
@@ -48,15 +40,28 @@ impl MyEguiApp {
 
 impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(
-            ctx,
-            |ui| {
-                ui.heading("Hello World!");
-                ui.image(
-                    self.tex.id(),
-                    self.tex.size_vec2(),
-                )
-            },
-        );
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+
+            let sin: PlotPoints = (0..1000)
+                .map(|i| {
+                    let x = i as f64 * 0.01;
+                    [x, x.sin()]
+                })
+                .collect();
+            let line = Line::new(sin);
+            let plot_image = PlotImage::new(
+                self.tex.id(),
+                egui::plot::PlotPoint { x: 0., y: 0. },
+                Vec2::new(self.tex.aspect_ratio(), 1.),
+            );
+            Plot::new("my_plot").view_aspect(1.0).data_aspect(1.).show(
+                ui,
+                |plot_ui| {
+                    plot_ui.image(plot_image);
+                    plot_ui.line(line)
+                },
+            );
+        });
     }
 }
