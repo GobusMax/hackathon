62a0dec30e3d670f171127604a191dd1c3bed5f1
Alexander Thiem <thiemalexander@outlook.de>
2023-05-06 09:46:06 +0200

    asdf

--- src/bin/detect.rs
+++ src/bin/detect.rs
@@ -1,5 +1,13 @@
-use image::{DynamicImage, ImageBuffer, Rgb, RgbImage};
+use image::{open, DynamicImage, GenericImageView, ImageBuffer, Rgb, RgbImage};
 
+pub fn airplane(a: &DynamicImage, b: &DynamicImage) -> (u64, u64) {
+    let diff = difference(&a.clone().into_rgb8(), &b.clone().into_rgb8());
+    diff.save("Difference.jpg").unwrap();
+    let diff = open("Difference.jpg").unwrap();
+    diff.grayscale().save("GrayscaleDifference.jpg").unwrap();
+    let diff = open("GrayscaleDifference.jpg").unwrap();
+    return average(&diff);
+}
 fn h(pix: &Rgb<u8>) -> u64 {
     let (x, y, z) = (pix.0[0] as u64, pix.0[1] as u64, pix.0[2] as u64);
     return x * x + y * y + z * z;
@@ -7,19 +15,77 @@ fn h(pix: &Rgb<u8>) -> u64 {
 
 pub fn average(img: &DynamicImage) -> (u64, u64) {
     let img = img.to_rgb8();
-    let sum: u64 = img.enumerate_pixels().map(|(_x, _y, pix)| h(pix)).sum();
+    let xdim = img.width();
+    let ydim = img.height();
+    let avg: u64 = img
+        .enumerate_pixels()
+        .map(|(_x, _y, pix)| h(pix))
+        .sum::<u64>()
+        / (xdim as u64 * ydim as u64);
+    let sum: u64 = img
+        .enumerate_pixels()
+        .filter(|(x, y, _pix)| {
+            *x > 0
+                && *y > 0
+                && *x + 1 < xdim
+                && *y + 1 < ydim
+                && h(img.get_pixel(x - 1, y - 1))
+                    + h(img.get_pixel(x - 1, *y))
+                    + h(img.get_pixel(x - 1, y + 1))
+                    + h(img.get_pixel(*x, y - 1))
+                    + h(img.get_pixel(*x, *y))
+                    + h(img.get_pixel(*x, y + 1))
+                    + h(img.get_pixel(x + 1, y - 1))
+                    + h(img.get_pixel(x + 1, *y))
+                    + h(img.get_pixel(x + 1, y + 1))
+                    > 9 * avg
+        })
+        .map(|(_x, _y, pix)| h(pix))
+        .sum();
     let x: u64 = img
         .enumerate_pixels()
+        .filter(|(x, y, _pix)| {
+            *x > 0
+                && *y > 0
+                && *x + 1 < xdim
+                && *y + 1 < ydim
+                && h(img.get_pixel(x - 1, y - 1))
+                    + h(img.get_pixel(x - 1, *y))
+                    + h(img.get_pixel(x - 1, y + 1))
+                    + h(img.get_pixel(*x, y - 1))
+                    + h(img.get_pixel(*x, *y))
+                    + h(img.get_pixel(*x, y + 1))
+                    + h(img.get_pixel(x + 1, y - 1))
+                    + h(img.get_pixel(x + 1, *y))
+                    + h(img.get_pixel(x + 1, y + 1))
+                    > 9 * avg
+        })
         .map(|(x, _y, pix)| x as u64 * h(pix))
         .sum();
     let y: u64 = img
         .enumerate_pixels()
+        .filter(|(x, y, _pix)| {
+            *x > 0
+                && *y > 0
+                && *x + 1 < xdim
+                && *y + 1 < ydim
+                && h(img.get_pixel(x - 1, y - 1))
+                    + h(img.get_pixel(x - 1, *y))
+                    + h(img.get_pixel(x - 1, y + 1))
+                    + h(img.get_pixel(*x, y - 1))
+                    + h(img.get_pixel(*x, *y))
+                    + h(img.get_pixel(*x, y + 1))
+                    + h(img.get_pixel(x + 1, y - 1))
+                    + h(img.get_pixel(x + 1, *y))
+                    + h(img.get_pixel(x + 1, y + 1))
+                    > 9 * avg
+        })
         .map(|(_x, y, pix)| y as u64 * h(pix))
         .sum();
     return (x / sum, y / sum);
 }
 
-pub fn difference2(
+pub fn difference(
     img: &RgbImage,
     img2: &RgbImage,
 ) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,28 @@
-use egui::vec2;
-use image::open;
+use egui::{vec2, Vec2};
+use image::{open, DynamicImage};
 #[path = "bin/detect.rs"]
 mod detect;
 mod image_manipulation;
 #[path = "bin/visualization.rs"]
 mod vis;
-
-fn main() {
-    let a = open("data/plane_2_2.jpg").unwrap();
-    let b = open("data/no_plane_2.jpg").unwrap();
-    let diff = detect::difference2(&a.clone().into_rgb8(), &b.into_rgb8());
-    diff.save("Difference.jpg").unwrap();
-    let diff = open("Difference.jpg").unwrap();
-    diff.grayscale().save("GrayscaleDifference.jpg").unwrap();
-    let diff = open("GrayscaleDifference.jpg").unwrap();
-    let x = detect::average(&diff);
-    let data = vec![
-        vec2(x.0 as f32, a.height() as f32 - x.1 as f32),
-        vec2(0., 0.),
-    ];
+fn display(img: DynamicImage, data: Vec<Vec2>) {
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "My egui App",
         native_options,
-        Box::new(|cc| Box::new(vis::MyEguiApp::new(cc, a, data))),
+        Box::new(|cc| Box::new(vis::MyEguiApp::new(cc, img, data))),
     )
     .unwrap();
-    //let a = image_manipulation::to_grayscale(a);
-    //let b = image_manipulation::to_grayscale(b);
-    //dbg!("Grayscaled");
-    //let res = detect::average(detect::difference(a, b));
-    //dbg!(res);
-    //let a = open("data/plane.jpg").unwrap();
+}
+fn main() {
+    let mut data = vec![];
+    for i in 2..52 {
+        let a = open(format!("data/short/{:03}.png", i)).unwrap();
+        let b = open("data/short/001.png").unwrap();
+        let x = detect::airplane(&a, &b);
+        data.push(vec2(x.0 as f32, a.height() as f32 - x.1 as f32));
+    }
+    let a = open("data/short/001.png").unwrap();
+
+    display(a, data);
 }
